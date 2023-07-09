f6ae770d89faa7376829939a54f3a674055c63eb
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-07-09 16:33:24 -0500

    update zig_parser version

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Parses Zig files and transforms them into ExDoc documentation.
 Note: ZigDoc is pinned to versions of ExDoc as it uses private
 features in ExDoc.
 
+This version of ZigDoc is pinned to zig 0.10.1
+
 ## Usage
 
 You'll want to alias the `mix docs` task to ZigDoc.  In your `mix.exs` file:
@@ -28,7 +30,7 @@ The package can be installed by adding `zig_doc` to your list of dependencies in
 ```elixir
 def deps do
   [
-    {:zig_doc, "~> 0.1.0"}
+    {:zig_doc, "~> 0.1.1"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ZigDoc.MixProject do
   def project do
     [
       app: :zig_doc,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -39,7 +39,7 @@ defmodule ZigDoc.MixProject do
       {:ex_doc, "== 0.29.4"},
       # this is also pinned to a version of zig_parser because
       # versions of zig_parser are pinned to zig versions.
-      {:zig_parser, "== 0.1.7"},
+      {:zig_parser, "== 0.1.8"},
       {:mox, "~> 1.0", only: :test}
     ]
   end
